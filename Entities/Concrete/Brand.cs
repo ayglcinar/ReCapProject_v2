9a0d32c08223907aa224365b996e5287b5a8d541
ayglcinar <60033267+ayglcinar@users.noreply.github.com>
2021-02-06 16:54:28 +0300

    Add files via upload

---
+++ Entities/Concrete/Brand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Entities.Abstract;
+
+namespace Entities.Concrete
+{
+    public class Brand : IEntity
+    {
+        public int BrandID { get; set; }
+        public string BrandName { get; set; }
+    }
+}
