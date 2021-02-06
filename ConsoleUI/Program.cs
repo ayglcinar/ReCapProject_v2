41db3405f689d523b1f0a7ea3e2e1e31c4671e0f
ayglcinar <60033267+ayglcinar@users.noreply.github.com>
2021-02-06 16:53:39 +0300

    Add files via upload

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var x in carManager.GetAll())
+            {
+                Console.WriteLine(x.Description);
+            }
+            
+        }
+    }
+}
