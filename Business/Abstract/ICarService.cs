a34e591bd056f21584d7e1ef731c273147ac1120
ayglcinar <60033267+ayglcinar@users.noreply.github.com>
2021-02-06 16:53:21 +0300

    Add files via upload

---
+++ Business/Abstract/ICarService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    public interface ICarService
+    {
+        List<Car> GetAll();
+        List<Car> GetCarsByColorId(int id);
+        List<Car> GetCarsByBrandId(int id);
+    }
+}
